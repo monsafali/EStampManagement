cfe435141b8629467e69d909753ca1b71f95132c
Asia Ashraf <asiaashraf7272@gmail.com>
2025-12-14 11:28:56 +0500

    fix action btn

--- frontend/src/pages/SuperAdminDashboard.jsx
+++ frontend/src/pages/SuperAdminDashboard.jsx
@@ -1,36 +1,63 @@
 
-
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../AuthContext";
 import CreateADCAdmin from './SuperAdmin/CreateADCAdmin';
 import ManageADCAdmins from "./SuperAdmin/ManageADCAdmins";
 import UploadRagForm from '../components/UploadRagForm';
-
+import "../styles/pages/SuperAdmin/SuperAdminDashboard.css";
 
 export default function SuperAdminDashboard() {
   const { user, loading } = useContext(AuthContext);
   // Track active section
   const [activeSection, setActiveSection] = useState("create"); // default create
 
-
   if (loading) return <p>Loading...</p>;
 
   return (
-    <div className="p-6 space-y-4">
-      <h1 className="text-3xl font-bold">
-        Welcome {user?.username} to Super Admin Dashboard
-      </h1>
-      {/* Logout Button */}
-      {/* <LogoutButton /> */}
-      <UploadRagForm/>
-    
-      {/* Change Password Section */}
-      {/* <ChangePassword /> */}
-      <CreateADCAdmin />
-      <h1>Get All Adc Admin</h1>
-      <ManageADCAdmins/>
+    <div className="sa-dashboard">
+      <div className="sa-header">
+        <h1>Super Admin Dashboard</h1>
+        <p>Welcome {user?.username}</p>
+      </div>
+      <div>
+        <UploadRagForm />
+      </div>
+      <div className="sa-actions">
+        <button
+          className={`sa-btn outline ${activeSection === "create" ? "active" : ""}`}
+          onClick={() => setActiveSection("create")}
+        >
+          Create ADC Admin
+        </button>
+
+        <button
+          className={`sa-btn outline ${activeSection === "manage" ? "active" : ""}`}
+          onClick={() => setActiveSection("manage")}
+        >
+          View / Manage ADC Admins
+        </button>
+      </div>
+      {/* CONDITIONALLY RENDER */}
+      {activeSection === "create" && (
+        <div className="sa-card">
+          {/* <h2>Create ADC Admin</h2> */}
+          <CreateADCAdmin />
+        </div>
+      )}
+
+      {activeSection === "manage" && (
+        <div className="sa-card">
+          <h2>Manage ADC Admins</h2>
+          <ManageADCAdmins />
+        </div>
+      )}
+
 
     </div>
+
+
+
+
   );
 }
 
